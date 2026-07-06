a8e7f55fbc82190af0e3c74a8413e9c969d1eedd
zzrftixx <andragiri.opy@gmail.com>
2026-07-06 18:28:23 +0700

    hello world

--- 1_hello_world.php
+++ 1_hello_world.php
@@ -1,12 +1,16 @@
-<!-- aplikasi pertama kita di php -->
+<!-- 
+aplikasi pertama kita di php 
+-->
 
 <?php
     echo "hello world."; //hello world application
+    
     echo "halo nama saya andra, ";
     echo "saya umur 22 tahun, ";
     echo "saya tinggal di banyuwangi, ";
     echo "saya memiliki minat untuk belajar pemrograman web.";
     echo " ";
+
     echo "php adalah banhasa pemrograman yang digunakan untuk membuat website dinamis. ";
     echo "dibuat oleh rasmus lerdorf pada tahun 1994. ";
     echo "php adalah bahasa pemrograman yang bersifat open source, sehingga dapat digunakan secara gratis. ";
--- 2_tipe_data_number.php
+++ 2_tipe_data_number.php
@@ -3,25 +3,29 @@ tipedata_number adalah tipe data yang digunakan untuk menyimpan nilai numerik at
 1. Integer (bilangan bulat): Tipe data ini digunakan untuk menyimpan bilangan bulat, baik positif maupun negatif. Contoh: 1, -5, 100, 0.
 2. Float (bilangan pecahan): Tipe data ini digunakan untuk menyimpan bilangan pecahan atau desimal. Contoh: 3.14, -0.5, 2.71828. 
 
-var_dump(); adalah fungsi bawaan PHP yang digunakan untuk menampilkan informasi tentang tipe data dan nilai dari sebuah variabel. Fungsi ini sangat berguna untuk debugging dan memahami struktur data dalam program PHP.
+var_dump(); adalah fungsi bawaan PHP yang digunakan untuk menampilkan informasi tentang tipe data dan nilai dari sebuah variabel. 
+Fungsi ini sangat berguna untuk debugging dan memahami struktur data dalam program PHP.
 -->
 
 
 <?php
-    echo "contoh tipe data number: ";
-    echo "integer, decimal:";
-    var_dump(123456789); // integer, decimal = menampilkan tipe data dan nilai dari variabel integer
+    echo "contoh tipe data number";
+    echo "integer, decimal: ";
+    var_dump(1234); // integer, decimal = menampilkan tipe data dan nilai dari variabel integer
     
-    echo "contoh tipe data number: ";
-    echo "integer, octal:";
-    var_dump(0123456789); // integer, octal = menampilkan tipe data dan nilai dari variabel integer
+    echo "contoh tipe data number";
+    echo "integer, octal: ";
+    var_dump(01234); // integer, octal = menampilkan tipe data dan nilai dari variabel integer
 
-    echo "contoh tipe data number: ";
-    echo "integer, hexadecimal:";
+    echo "contoh tipe data number";
+    echo "integer, hexadecimal: ";
     var_dump(0x1A); // integer, hexadecimal = menampilkan tipe data dan nilai dari variabel integer
     
-    echo "contoh tipe data number: ";
-    echo "integer, binary:";
-    var_dump(1234); // integer, binary = menampilkan tipe data dan nilai dari variabel integer
+    echo "contoh tipe data number";
+    echo "integer, binary: ";
+    var_dump(0b11111111); // integer, binary = menampilkan tipe data dan nilai dari variabel integer
     
+    echo "contoh tipe data number";
+    echo "integer, underscore di number: ";
+    var_dump(1_234_567); // integer, underscore di number = menampilkan tipe data dan nilai dari variabel integer
 ?>
