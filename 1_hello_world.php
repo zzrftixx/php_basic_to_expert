0f7fa9f0804c6b80bf43a73ce3b03db7cb7c9e5f
zzrftixx <andragiri.opy@gmail.com>
2026-07-06 18:07:13 +0700

    hello world

---
+++ 1_hello_world.php
@@ -0,0 +1,9 @@
+<!-- aplikasi pertama kita di php -->
+
+<?php
+    echo "hello world."; //hello world application
+    echo "halo nama saya andra, ";
+    echo "saya umur 22 tahun, ";
+    echo "saya tinggal di banyuwangi, ";
+    echo "saya memiliki minat untuk belajar pemrograman web.";
+?>
