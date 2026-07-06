6301cf79f31479fbdd85004604f1c0a2f4d7006c
zzrftixx <andragiri.opy@gmail.com>
2026-07-06 18:09:49 +0700

    hello world

--- 1_hello_world.php
+++ 1_hello_world.php
@@ -6,4 +6,8 @@
     echo "saya umur 22 tahun, ";
     echo "saya tinggal di banyuwangi, ";
     echo "saya memiliki minat untuk belajar pemrograman web.";
+    echo " ";
+    echo "php adalah banhasa pemrograman yang digunakan untuk membuat website dinamis. ";
+    echo "dibuat oleh rasmus lerdorf pada tahun 1994. ";
+    echo "php adalah bahasa pemrograman yang bersifat open source, sehingga dapat digunakan secara gratis. ";
 ?>
