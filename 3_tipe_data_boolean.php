74322582e63b1f1c0be95c7ac2c61dadad9f7c0f
zzrftixx <andragiri.opy@gmail.com>
2026-07-06 22:57:45 +0700

    boolean

---
+++ 3_tipe_data_boolean.php
@@ -0,0 +1,22 @@
+<!-- 
+disini kita belajar tipe data boolean pada php, tipe data boolean 
+hanya memiliki dua nilai yaitu true dan false. tipe data ini 
+biasanya digunakan untuk kondisi atau pernyataan logika. contoh 
+penggunaan tipe data boolean dalam php adalah sebagai berikut: 
+-->
+
+<?php
+
+echo "contoh tipe data number";
+echo "benar: ";
+var_dump(true); // benar = menampilkan tipe data dan nilai dari variabel boolean true
+var_dump(TRUE); 
+var_dump(1); // diwakilkan dengan angka 1, menampilkan tipe data dan nilai dari variabel boolean true
+
+echo "contoh tipe data number";
+echo "salah: ";
+var_dump(false); // salah = menampilkan tipe data dan nilai dari variabel boolean false
+var_dump(FALSE); 
+var_dump(0); // diwakilkan dengan angka 0, menampilkan tipe data dan nilai dari variabel boolean false
+
+?>
