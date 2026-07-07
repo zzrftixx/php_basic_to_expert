ccca2455f9407d047849c96f4aa2a78c59896947
zzrftixx <andragiri.opy@gmail.com>
2026-07-08 06:04:40 +0700

    variable

--- 5_variable.php
+++ 5_variable.php
@@ -5,5 +5,7 @@
 <?php
 
 $int = 1234; // integer = menampilkan tipe data dan nilai dari variabel integer
-echo "ini angka $int";
+$string = "hallo"; // string = menampilkan tipe data dan nilai dari variabel string
+echo "ini angka $int dan ini string $string"; // menampilkan tipe data dan nilai dari variabel integer dan string
+echo "$$int"; // men
 ?>
