924b8011bb617842e96902ca821fae7774f577d1
zzrftixx <andragiri.opy@gmail.com>
2026-07-10 15:35:27 +0700

    hello world

--- 1_hello_world.php
+++ 1_hello_world.php
@@ -1,17 +1,8 @@
 <!-- 
-aplikasi pertama kita di php 
--->
+                        Hello world
+    Aplikasi pertama yang kita buat memakai bahasa PHP 
+                                                        -->
 
 <?php
-    echo "hello world."; //hello world application
-    
-    echo "halo nama saya andra, ";
-    echo "saya umur 22 tahun, ";
-    echo "saya tinggal di banyuwangi, ";
-    echo "saya memiliki minat untuk belajar pemrograman web.";
-    echo " ";
-
-    echo "php adalah banhasa pemrograman yang digunakan untuk membuat website dinamis. ";
-    echo "dibuat oleh rasmus lerdorf pada tahun 1994. ";
-    echo "php adalah bahasa pemrograman yang bersifat open source, sehingga dapat digunakan secara gratis. ";
+    echo "Hello World";
 ?>
