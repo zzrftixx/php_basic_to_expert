0b1b501d0a4a73b5060e2b00a634774999c7ffc2
zzrftixx <andragiri.opy@gmail.com>
2026-07-11 15:32:35 +0700

    Hello World

--- 1_hello_world.php
+++ 1_hello_world.php
@@ -1,7 +1,15 @@
 <!-- 
                         Hello world
-    Aplikasi pertama yang kita buat memakai bahasa PHP 
-                                                        -->
+    Aplikasi pertama yang kita buat memakai bahasa PHP adalah 
+    aplikasi Hello World. Aplikasi ini menampilkan teks 
+    "Hello World" ke layar.
+
+    Note:
+        var_dump(); adalah fungsi bawaan PHP yang di gunakan 
+        untuk menampilkan informasi tentang tipe data dan 
+        nilai dari sebuah variabel. Fungsi ini sangat berguna
+        untuk debugging
+                                                                -->
 
 <?php
     echo "Hello World";
