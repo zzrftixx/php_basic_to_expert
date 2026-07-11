4d2ec59909ccddfb41be1de1942559969646dfea
zzrftixx <andragiri.opy@gmail.com>
2026-07-11 16:28:09 +0700

    constant

---
+++ 6_constant.php
@@ -0,0 +1,26 @@
+<!-- 
+                            Constanta dan Variable
+
+    Variable adalah sebuah tempat untuk menyimpan data atau nilai sementara dalam
+    program. Variable dapat menyimpan berbagai jenis tipe data, seperti string,
+    integer, float, boolean, array, dan lain-lain. Variable dapat diubah nilainya
+    selama program berjalan, sehingga dapat digunakan untuk menyimpan data yang
+    bersifat dinamis. 
+
+    Namun ada yang unik di PHP, yaitu variable di PHP tidak perlu di deklarasikan 
+    tipe datanya, karena PHP adalah bahasa pemrograman yang bersifat loosely 
+    typed. Artinya, kita tidak perlu menentukan tipe data dari sebuah variable 
+    saat membuatnya. PHP akan secara otomatis menentukan tipe data dari variable 
+    berdasarkan nilai yang diberikan.
+
+    Dan ada juga namnanya variable variables, yaitu sebuah variable yang namanya 
+    diambil dari nilai variable lain.
+
+    Note:
+        contoh variable di PHP selalu diawali dengan tanda dollar ($) diikuti 
+        dengan nama variable.
+                                                                                    -->
+
+<?php
+    
+?>
