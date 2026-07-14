8533e33384d22ade03b96f3f6e46e0ea970200d6
zzrftixx <andragiri.opy@gmail.com>
2026-07-14 13:22:17 +0700

    tipe data array

---
+++ 8_tipe_data_array.php
@@ -0,0 +1,14 @@
+<!-- 
+                                    Tipe Data Array
+                                    
+    
+
+    Note:
+        
+    
+    list function:
+                                                                                          -->
+
+<?php
+    echo " --------------------- Tipe Data Array --------------------- \n\n";
+?>
